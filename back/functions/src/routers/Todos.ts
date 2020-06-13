859662caef767237c5cb3df40cf2febcf3ffb8d1
habacuqueboy@Protonmail.ch <habacuqueboy@Protonmail.ch>
2020-06-13 14:33:45 -0300

    functions CRUD

--- back/functions/src/routers/Todos.ts
+++ back/functions/src/routers/Todos.ts
@@ -4,6 +4,17 @@ import {firestore} from "firebase-admin"
 const router = Router()
 const db = firestore().collection("todos")
 
+//Create
+router.post('/',async (req, res) => {
+    try {
+        await db.add(req.body)
+        res.status(200).json(req.body)
+    } catch (e) {
+        res.status(400).json({err:e})
+    }
+})
+
+//Read
 router.get('/',async (req,res) => {
     try {
         const docs = await db.get()
@@ -15,14 +26,29 @@ router.get('/',async (req,res) => {
    } 
 })
 
-router.post('/',async (request, response) => {
+//Update
+router.put('/',async (req,res) => {
+    const id = req.body.id
     try {
-        await db.add(request.body)
-        response.status(200).json(request.body)
-    } catch (e) {
-        response.status(400).json({err:e})
+        await db.doc(id).update(req.body)
+        res.status(200).json(req.body)
+    } catch(e) {
+        res.status(400).json({err:e})
     }
 })
+
+
+//Delete
+router.delete('/',async (req,res) => {
+    const id = req.body.id
+    try {
+        await db.doc(id).delete()
+        res.status(200).json(req.body)
+    } catch(e) {
+        res.status(400).json({err:e})
+    }
+})
+
 
 
 export const Todos = router 
